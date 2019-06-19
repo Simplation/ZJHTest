e1a74c0c5ccf04ad3ea6cc7232d4da80e6d80a5d
Simplation.WANG <simplationwang@gmail.com>
2019-06-19 14:57:12 +0800

    Add files via upload

---
+++ zjhlibrary/src/main/java/com/sunnyit/zjhlibrary/EmptyLoginService.java
@@ -0,0 +1,10 @@
+package com.sunnyit.zjhlibrary;
+
+import android.content.Context;
+
+public class EmptyLoginService implements ILoginService {
+    @Override
+    public void launch(Context context, String targetClass) {
+
+    }
+}
